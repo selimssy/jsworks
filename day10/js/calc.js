6ffe3715323ae8afbd8967688a3d7cbb932a598d
selimssy <selimssy@naver.com>
2022-05-20 18:18:36 +0900

    자바스크립트 소스 day10

---
+++ day10/js/calc.js
@@ -0,0 +1,11 @@
+// 덧셈 프로그램
+
+let n1 = parseInt(prompt("첫 번째 수 입력 :"));
+let n2 = parseInt(prompt("두 번째 수 입력 :"));
+let sum =0;
+
+
+sum = n1 + n2;
+// sum = parseInt(n1) + parseInt(n2);
+
+document.write("두 수의 합 : " + sum);
