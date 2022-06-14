34b176ed99aeeb010c5e64fb81437ed84b5e4c4d
selimssy <selimssy@naver.com>
2022-06-14 18:21:13 +0900

    day25

--- hi-tour/js/common.js
+++ hi-tour/js/common.js
@@ -2,81 +2,268 @@
 
 $(document).ready( function() {
 
+	// let isOver1 = false    // 상위메뉴(마우스오버되면 : true)
+	// let isOver2 = false   // 하위메뉴(마우스오버되면 : true)
+
+	let isOver1 = [false, false, false, false]
+	let isOver2 = [false, false, false, false]
+
 	// fadeOut() 시간 설정
 	function goHide(){
-		for(let i=1; i <=4; i++){
-			$(".gnb_depth2_" + i).fadeOut("fast")
+		for(let i=0; i <4; i++){
+			if(!isOver1[i] && !isOver2[i])  // 상위와 하위 둘 다 마우스가 닿았을 때만      // 깜빡이는거 없어진다
+			$(".gnb_depth2_" + (i+1)).fadeOut("fast")
 		}
 	}
 
 
+
+	for(let i = 1; i <=4; i++){
+		$(".openAll" + i).mouseover(function(){
+			// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+			if(parseInt($("header").css("width")) > 800){   // parseInt 해줘야 인식한다!!
+				$(".gnb_depth2_" + i).fadeIn("fast")
+				isOver1[i - 1] = true
+			}
+		}).focus(function(){
+			if(parseInt($("header").css("width")) > 800){
+				$(".gnb_depth2_" + i).fadeIn("fast")
+				isOver1[i - 1] = true
+			}
+		}).mouseout(function(){
+			// $(".gnb_depth2_1").fadeOut("fast")
+			setTimeout(goHide, 200)  // 0.5초 뒤에 사라지도록
+			isOver1[i - 1] = false
+		}).blur(function(){
+			setTimeout(goHide, 200)    // mouseover는 mouseout으로, focus는 blur로
+			isOver1[i - 1] = false
+	
+		})
+	
+	
+		// 하위메뉴
+		$(".gnb_depth2_" + i).mouseover(function(){
+			isOver2[i - 1] = true
+		}).focus(function(){
+			isOver2[i - 1] = true
+		}).mouseout(function(){
+			isOver2[i - 1] = false
+			setTimeout(goHide, 200)
+		}).blur(function(){
+			isOver2[i - 1] = false
+			setTimeout(goHide, 200)
+		})
+	}
+
+
+
+
+
+
 	// 상단 메뉴 마우스오버, 탭 클릭 코드
-	$(".openAll1").mouseover(function(){
-		// 서브메뉴 나타나기(header 너비가 800보다 클 때만
-		if(parseInt($("header").css("width")) > 800)   // parseInt 해줘야 인식한다!!
-			$(".gnb_depth2_1").fadeIn("fast")
-	}).focus(function(){
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_1").fadeIn("fast")
-	}).mouseout(function(){
-		// $(".gnb_depth2_1").fadeOut("fast")
-		setTimeout(goHide, 500)  // 0.5초 뒤에 사라지도록
-	}).blur(function(){
-		setTimeout(goHide, 500)    // mouseover는 mouseout으로, focus는 blur로
-	})
+	// 상위메뉴
+	// $(".openAll1").mouseover(function(){
+	// 	// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+	// 	if(parseInt($("header").css("width")) > 800){   // parseInt 해줘야 인식한다!!
+	// 		$(".gnb_depth2_1").fadeIn("fast")
+	// 		isOver1[0] = true
+	// 	}
+	// }).focus(function(){
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_1").fadeIn("fast")
+	// 		isOver1[0] = true
+	// 	}
+	// }).mouseout(function(){
+	// 	// $(".gnb_depth2_1").fadeOut("fast")
+	// 	setTimeout(goHide, 500)  // 0.5초 뒤에 사라지도록
+	// 	isOver1[0] = false
+	// }).blur(function(){
+	// 	setTimeout(goHide, 500)    // mouseover는 mouseout으로, focus는 blur로
+	// 	isOver1[0] = false
 
-	$(".openAll2").mouseover(function(){
-		// 서브메뉴 나타나기(header 너비가 800보다 클 때만
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_2").fadeIn("fast")
-	}).focus(function(){
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_2").fadeIn("fast")
-	}).mouseout(function(){
-		// $(".gnb_depth2_2").fadeOut("fast")
-		setTimeout(goHide, 500)
-	}).blur(function(){
-		setTimeout(goHide, 500)
-	})
+	// })
 
-	$(".openAll3").mouseover(function(){
-		// 서브메뉴 나타나기(header 너비가 800보다 클 때만
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_3").fadeIn("fast")
-	}).focus(function(){
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_3").fadeIn("fast")
-	}).mouseout(function(){
-		// $(".gnb_depth2_3").fadeOut("fast")
-		setTimeout(goHide, 500)
-	}).blur(function(){
-		setTimeout(goHide, 500)
-	})
 
-	$(".openAll4").mouseover(function(){
-		// 서브메뉴 나타나기(header 너비가 800보다 클 때만
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_4").fadeIn("fast")
-	}).focus(function(){
-		if(parseInt($("header").css("width")) > 800)
-			$(".gnb_depth2_4").fadeIn("fast")
-	}).mouseout(function(){
-		// $(".gnb_depth2_4").fadeOut("fast")
-		setTimeout(goHide, 500)
-	}).blur(function(){
-		setTimeout(goHide, 500)
-	})
+	// // 하위메뉴
+	// $(".gnb_depth2_1").mouseover(function(){
+	// 	isOver2[0] = true
+	// }).focus(function(){
+	// 	isOver2[0] = true
+	// }).mouseout(function(){
+	// 	isOver2[0] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver2[0] = false
+	// 	setTimeout(goHide, 500)
+	// })
 
-	$(".gnb_depth").mouseover(function(){
-		$(this).show()    // 대고있으면 계속 보이도록 하고싶다ㅠ
-	})
+
+
+
+
+	
+
+
+
+
+	// $(".openAll2").mouseover(function(){
+	// 	// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_2").fadeIn("fast")
+	// 		isOver1[1] = true
+	// 	}
+	// }).focus(function(){
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_2").fadeIn("fast")
+	// 		isOver1[1] = true
+	// 	}
+	// }).mouseout(function(){
+	// 	// $(".gnb_depth2_2").fadeOut("fast")
+	// 	isOver1[1] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver1[1] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+
+	// $(".gnb_depth2_2").mouseover(function(){
+	// 	isOver2[1] = true
+	// }).focus(function(){
+	// 	isOver2[1] = true
+	// }).mouseout(function(){
+	// 	isOver2[1] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver2[1] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+
+
+
+
+	// $(".openAll3").mouseover(function(){
+	// 	// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_3").fadeIn("fast")
+	// 		isOver1[2] = true
+	// 	}
+	// }).focus(function(){
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_3").fadeIn("fast")
+	// 		isOver1[2] = true
+	// 	}
+	// }).mouseout(function(){
+	// 	// $(".gnb_depth2_3").fadeOut("fast")
+	// 	isOver1[2] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver1[2] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+	// $(".gnb_depth2_3").mouseover(function(){
+	// 	isOver2[2] = true
+	// }).focus(function(){
+	// 	isOver2[2] = true
+	// }).mouseout(function(){
+	// 	isOver2[2] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver2[2] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+
+
+
+	// $(".openAll4").mouseover(function(){
+	// 	// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_4").fadeIn("fast")
+	// 		isOver1[3] = true
+	// 	}
+	// }).focus(function(){
+	// 	if(parseInt($("header").css("width")) > 800){
+	// 		$(".gnb_depth2_4").fadeIn("fast")
+	// 		isOver1[3] = true
+	// 	}
+	// }).mouseout(function(){
+	// 	// $(".gnb_depth2_4").fadeOut("fast")
+	// 	isOver1[3] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver1[3] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+	// $(".gnb_depth2_4").mouseover(function(){
+	// 	isOver2[3] = true
+	// }).focus(function(){
+	// 	isOver2[3] = true
+	// }).mouseout(function(){
+	// 	isOver2[3] = false
+	// 	setTimeout(goHide, 500)
+	// }).blur(function(){
+	// 	isOver2[3] = false
+	// 	setTimeout(goHide, 500)
+	// })
+
+
+
+
+
+
+	// for(let i = 1; i <= 4; i++){
+	// 	$(".openAll" + i).mouseover(function(){
+	// 		// 서브메뉴 나타나기(header 너비가 800보다 클 때만
+	// 		if(parseInt($("header").css("width")) > 800){
+	// 			$(".gnb_depth2_" + i).fadeIn("fast")
+	// 			isOver1 = true
+	// 		}
+	// 	}).focus(function(){
+	// 		if(parseInt($("header").css("width")) > 800){
+	// 			$(".gnb_depth2_" + i).fadeIn("fast")
+	// 			isOver1 = true
+	// 		}
+	// 	}).mouseout(function(){
+	// 		// $(".gnb_depth2_4").fadeOut("fast")
+	// 		isOver1 = false
+	// 		setTimeout(goHide, 500)
+	// 	}).blur(function(){
+	// 		isOver1 = false
+	// 		setTimeout(goHide, 500)
+	// 	})
+	
+	// 	$(".gnb_depth2_" + i).mouseover(function(){
+	// 		isOver2 = true
+	// 	}).focus(function(){
+	// 		isOver2 = true
+	// 	}).mouseout(function(){
+	// 		isOver2 = false
+	// 		setTimeout(goHide, 500)
+	// 	}).blur(function(){
+	// 		isOver2 = false
+	// 		setTimeout(goHide, 500)
+	// 	})
+	
+	// }
+
+
+
+	
+
+
+	
+
 
 	// top을 클릭하면 header로 이동(라이브러리)
-	$('a.s_point').smoothScroll();
+	$('a.s_point').smoothScroll();   // 이 줄은 없어도 작동된다
 
 	// 스크롤하여 내려와야 top버튼이 보이도록 함.
-	if ( $(document).scrollTop() < 50) $('.to_top').addClass('hide');
-	else $('.to_top').removeClass('hide');
+	// if ( $(document).scrollTop() < 50) $('.to_top').addClass('hide');
+	// else $('.to_top').removeClass('hide');     // 이거 없어도 되는 것 같다
 	$(window).scroll( function() {
 	  if ( $(document).scrollTop() < 50) $('.to_top').addClass('hide');
 	  else $('.to_top').removeClass('hide');
--- hi-tour/js/rollmain.js
+++ hi-tour/js/rollmain.js
@@ -1,62 +1,83 @@
+// 이미지 슬라이드 구현
+let  num = 0   // 이미지 번호
+let slidingAuto = null;   // 슬라이드 기능 작동시키는데 사용할 객체
 
- 	var wnum=0;
-	var wwslidingAuto=null;
-	function play_w(directw) {	
-			if(directw=="right"){
-				wnum++;
-				if(wnum>2)wnum=0;
-			}else if(directw=="left"){
-				wnum--;
-				if(wnum<0)wnum=2;
-			}else {
-				wnum = directw;
-			}
-			
-			$(".rollingbtn").find('li.seq a').each(function(){
-				$(".rollingbtn li.seq a img").attr('src',$(".rollingbtn li.seq a img").attr('src').replace('_on.png','_off.png'));
-			});
-			$(".rollingbtn li.butt"+wnum+ " a img").attr('src',$(".rollingbtn li.butt"+wnum+ " a img").attr('src').replace('_off.png','_on.png'));
 
-				if(wnum==0) {
-				$(".viewImgList li.imglist1").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist0").animate({'opacity' : 1 },1000);
-			}else if (wnum==1){
-				$(".viewImgList li.imglist0").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist1").animate({'opacity' : 1 },1000);
-			}else if (wnum==2){
-				$(".viewImgList li.imglist0").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist1").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 1 },1000);
-			}
 
-			if(wwslidingAuto)clearTimeout(wwslidingAuto);
-			wwslidingAuto=setTimeout("play_w('right')",5000);
+
+// 슬라이드 3초 간격으로 작동
+slidingAuto = setTimeout("play('right')", 2000)    // 매개변수 있으면 따음표 해야!!
+
+function play(direct){   // 방향 매개 변수 설정
+	if(direct = "right"){
+		num++
+		if(num > 2)
+			num = 0
 	}
-	var wwslidingAuto=setTimeout("play_w('right')",5000);
+	else if(direct = "left"){
+		num--
+		if(num < 0)
+			num = 2
+	}
+	// else{
+	// 	num = direct      // 이게 뭔소리지    // 필요 없을 것 같다
+	// }
 
-$(document).ready(function(){
 
-	$(".rollstop a").click(function(){
-		$(this).parent().hide();
-		$(".rollplay").css('display','inline-block');
-		if(wwslidingAuto)clearTimeout(wwslidingAuto);	
-	});
 
+	// 각각의 버튼이 on - > off로 전체적으로 순환
+	$(".rollingbtn").find("li.seq a").each(function(){
+		$(".rollingbtn li.seq a img").attr("src", $(".rollingbtn li.seq a img").attr("src").replace("_on.png", "_off.png"))   // "_on.png"에서 "_off.png"로 교체
+	})
+
+	
+
+	// 하나의 버튼이 on -> off로 변경
+	$(".rollingbtn li.butt" + num + " a img").attr("src", $(".rollingbtn li.butt" + num + " a img").attr("src").replace("_off.png", "_on.png"))
+
+
+
+
+	if(num == 0){
+		$(".viewImgList li.imglist1").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist2").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist0").animate({"opacity" : 1}, 1000)   // 0번 이미지만 보이고 나머지는 불투명해서 안보이도록		
+	}
+	else if(num == 1){
+		$(".viewImgList li.imglist2").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist0").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist1").animate({"opacity" : 1}, 1000)   	
+	}
+	else if(num == 2){
+		$(".viewImgList li.imglist0").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist1").animate({"opacity" : 0}, 1000)
+		$(".viewImgList li.imglist2").animate({"opacity" : 1}, 1000)  		
+	}
+
+	
+	slidingAuto = setTimeout("play('right')", 2000)     // 콜백함수 해줘야 계속 돌아간다
+													    // 변수에 넣어줘야 뒤에서 slidingAuto를 clearTimeout 할 수 있다
+	
+
+	
+}
+
+
+
+
+// 재생과 일시정지 구현
+$(function(){
+	// 정지버튼 클릭하는 순간 정지 버튼은 사라지고 재생버튼 보이도록
+	$(".rollstop a").click(function(){		
+		$(this).parent().hide()    
+		$(".rollplay").css("display", "inline-block")
+		if(slidingAuto)   // slidingAuto가 존재하면(작동하고 있으면)
+			clearTimeout(slidingAuto)    // 정지
+	})
+	// 재생버튼 클릭하는 순간 재생 버튼은 사라지고 정지버튼 보이도록
 	$(".rollplay a").click(function(){
-		$(this).parent().hide();
-		$(".rollstop").css('display','inline-block');
-		play_w('right');
-	});
-
-	$(".rollingbtn li.seq a").each(function(index){
-		$(this).click(function(){
-			$(".rollplay").hide();
-			$(".rollstop").css('display','inline-block');
-			if(wwslidingAuto)clearTimeout(wwslidingAuto);	
-			play_w(index);
-		});
-	});
-
-});
+		$(this).parent().hide()    
+		$(".rollstop").css("display", "inline-block")
+		play("right")
+	})
+})
